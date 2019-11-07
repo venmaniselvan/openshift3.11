3fa2c72fa035796fb81e75caa83a98351c156552
venmaniselvan <53399998+venmaniselvan@users.noreply.github.com>
2019-11-07 18:33:27 +0530

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -2,10 +2,10 @@
 
 mkdir -p /etc/origin/master/
 touch /etc/origin/master/htpasswd
-[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
+[ ! -d openshift3.11 ] && git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
 
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml -vvv
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml -vvv
+ansible-playbook -i inventory.ini openshift3.11/playbooks/prerequisites.yml -vvv
+ansible-playbook -i inventory.ini openshift3.11/playbooks/deploy_cluster.yml -vvv
 
 htpasswd -b /etc/origin/master/htpasswd admin admin123
 oc adm policy add-cluster-role-to-user cluster-admin admin
