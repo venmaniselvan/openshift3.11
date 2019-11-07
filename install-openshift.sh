c08db6f2b012aee643f42819374480d5b4b9bec1
venmaniselvan <53399998+venmaniselvan@users.noreply.github.com>
2019-11-07 18:30:29 +0530

    Create install-openshift.sh

---
+++ install-openshift.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p /etc/origin/master/
+touch /etc/origin/master/htpasswd
+[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
+
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml -vvv
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml -vvv
+
+htpasswd -b /etc/origin/master/htpasswd admin admin123
+oc adm policy add-cluster-role-to-user cluster-admin admin
